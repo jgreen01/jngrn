a4e42f26feb24ab57a5dc8d8dec57bcaa5ad4ba0
Jon Green <jgreen82@gatech.edu>
2016-01-09 16:15:32 -0500

    Reconfigured gulp tasks and added new ones. Project will now livereload and compile jade. I have a much better idea of how these projects need to be layed out. I'm glad I made the gulp file so easy to update.

--- gulpfile.js
+++ gulpfile.js
@@ -1,85 +1,93 @@
 /** gulp build tools **/
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var sass = require('gulp-sass');
-var plumber = require('gulp-plumber');
-var imagemin = require('gulp-imagemin');
-var prefix = require('gulp-autoprefixer');
-var minifyCss = require('gulp-minify-css');
-var copy = require('recursive-copy');
+var gulp = require('gulp'),
+		uglify = require('gulp-uglify'),
+		sass = require('gulp-sass'),
+		plumber = require('gulp-plumber'),
+		imagemin = require('gulp-imagemin'),
+		prefix = require('gulp-autoprefixer'),
+		minifyCss = require('gulp-minify-css'),
+		connect = require('gulp-connect'),
+		jshint = require('gulp-jshint'),
+		stylish = require('jshint-stylish'),
+		htmlmin = require('gulp-htmlmin'),
+		jade = require('gulp-jade');
 
 /** Source location **/
-var src_location = 'src/';
-var node_modules = 'node_modules/'
+var src_location = 'source/';
+var bower_src = 'bower_components/'
 var styles_src = src_location + 'sass/**/*.s[ac]ss';
-var jscripts_src = src_location + 'js/**/*.js';
-var images_src = src_location + 'img/**/*';
+var jscripts_src = src_location + 'javascript/**/*.js';
+var images_src = src_location + 'images/**/*';
+var templates_src = src_location + '*.jade';
 
 /** Destination location **/
-var dest_location = 'public/';
-var styles_dest = dest_location + 'stylesheets';
-var jscripts_dest = dest_location + 'javascripts';
-var images_dest = dest_location + 'images';
+var dest_location = 'www/';
+var styles_dest = dest_location + 'css';
+var jscripts_dest = dest_location + 'js';
+var images_dest = dest_location + 'img';
+var templates_dest = dest_location;
 
 /***  Build Tasks  ***/
 
-gulp.task('default', ['scripts','styles','images']);
+gulp.task('build', ['scripts','styles','templates','images']);
+gulp.task('default', ['build','watch','connect']);
 
-/** Scripts task - All things done to the JavaScript files
- *    - Uglifies
- */
 gulp.task('scripts', function() {
-  gulp.src(jscripts_src)
-    .pipe(plumber())
-    .pipe(uglify())
-    .pipe(gulp.dest(jscripts_dest));
+	return gulp.src(jscripts_src)
+			.pipe(plumber())
+			.pipe(jshint())
+			.pipe(jshint.reporter())
+			.pipe(uglify())
+			.pipe(gulp.dest(jscripts_dest))
+			.pipe(connect.reload());
 });
 
-/** Styles task - All things done to the style files
- */
 gulp.task('styles', function() {
-  gulp.src(styles_src)
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(prefix('last 2 versions'))
-    .pipe(minifyCss())
-    .pipe(gulp.dest(styles_dest));
+	return gulp.src(styles_src)
+			.pipe(plumber())
+			.pipe(sass())
+			.pipe(prefix('last 2 versions'))
+			.pipe(minifyCss())
+			.pipe(gulp.dest(styles_dest))
+			.pipe(connect.reload());
 });
 
-/** Images task - All things done to the style files
- */
+gulp.task('templates', function() {
+	return gulp.src(templates_src)
+			.pipe(plumber())
+			.pipe(jade({
+				locals: {}
+			}))
+			.pipe(htmlmin({collapseWhitespace: true}))
+			.pipe(gulp.dest(templates_dest))
+			.pipe(connect.reload());
+});
+
 gulp.task('images', function() {
-  gulp.src(images_src)
-    .pipe(imagemin())
-    .pipe(gulp.dest(images_dest));
+	return gulp.src(images_src)
+			.pipe(imagemin())
+			.pipe(gulp.dest(images_dest))
+			.pipe(connect.reload());
 });
 
-/***  Watch Tasks  ***/
-
-/** Watch scripts task
- *    - watches js
- */
-gulp.task('watch:scripts', function() {
-  gulp.watch(jscript_location, ['scripts']);
-});
-
-/** Watch styles task
- *    - watches styl
- */
-gulp.task('watch:styles', function() {
-  gulp.watch(styles_src, ['styles']);
-});
-
-/** Watch all task
- *    watches js and styl
- */
-gulp.task('watch:all', function() {
+/** Watch task */
+gulp.task('watch', function() {
   gulp.watch(styles_src, ['styles']);
   gulp.watch(jscripts_src, ['scripts']);
+	gulp.watch(templates_src, ['templates']);
+	gulp.watch(images_src, ['images']);
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'www',
+    livereload: true
+  });
 });
 
 /***  Setup Tasks  ***/
 
+/*
 gulp.task('setup:styles', function() {
   copy('node_modules/flexboxgrid-sass/source/sass/', 'src/sass/flexboxgrid/', function(err) {
     if (err) console.error('Copy failed: ' + err);
@@ -91,3 +99,4 @@ gulp.task('setup:styles', function() {
     else console.info('Materialize is ready');
   });
 });
+*/
