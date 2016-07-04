7f2289d44b2aefc7416416d5c4b8e506fc913f28
Jon Green <greenbj@umich.edu>
2016-07-04 17:30:41 +0000

    Adding Material-UI

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
+
 import { Provider } from 'react-redux';
+
 import configureStore from './stores';
 import App from './containers/App';
+
+import { MuiThemeProvider } from 'material-ui/styles';
+
+// may or may not need 'react-tap-event-plugin'
+// https://github.com/callemall/material-ui#react-tap-event-plugin
+// http://stackoverflow.com/a/34015469/988941
 
 const store = configureStore();
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+ <MuiThemeProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('app')
 );
