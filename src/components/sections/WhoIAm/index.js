393b4052e6f298b44986403dd5034340496ddcc2
Jon Green <greenbj@umich.edu>
2016-07-12 23:37:57 +0000

    Adding PureRenderMixin and immutable.

--- src/components/sections/WhoIAm/index.js
+++ src/components/sections/WhoIAm/index.js
@@ -1,10 +1,16 @@
 require('styles/sections/WhoIAm/WhoIAm.scss');
 
 import React, { Component, PropTypes } from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 export default class WhoIAm extends Component {
 
 	static propTypes = {};
+
+	constructor(props) {
+		super(props);
+		this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+	}
 
 	render() {
 		return (
