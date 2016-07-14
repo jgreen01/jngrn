a2dc0a60f220694b4f5dcf9f1b47748c847f611e
Jon Green <greenbj@umich.edu>
2016-07-14 23:46:28 +0000

    Adding Doorway components to the WhoIAm component. Also adding images that may or may not be used in the final product.

--- src/components/sections/WhoIAm/index.js
+++ src/components/sections/WhoIAm/index.js
@@ -3,6 +3,12 @@ require('styles/sections/WhoIAm/WhoIAm.scss');
 import React, { Component, PropTypes } from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 
+import Doorway from './Doorway';
+
+const hippoPic = require('images/Hippo_mouth_opening.jpg');
+const footsteps = require('images/Footsteps.jpg');
+const KLC = require('images/Knights_of_the_Lambda_Calculus.svg');
+
 export default class WhoIAm extends Component {
 
 	static propTypes = {};
@@ -11,10 +17,33 @@ export default class WhoIAm extends Component {
 		super(props);
 		this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
 	}
+	
+	componentWillMount() {
+		this.setState({
+			bgImage: hippoPic
+		});
+	}
 
 	render() {
 		return (
-			<div className="aboutme-component">
+			<div
+				className="aboutme-component"
+				style={ {
+					backgroundImage: `url(${bgImage})`
+				} }
+			>
+				<Doorway
+					image={ hippoPic }
+					onMouseOver={ event => this.setState({ bgImage: hippoPic }) }
+				/>
+				<Doorway
+					image={ footsteps }
+					onMouseOver={ event => this.setState({ bgImage: footsteps }) }
+				/>
+				<Doorway
+					image={ KLC }
+					onMouseOver={ event => this.setState({ bgImage: KLC }) }
+				/>
 			</div>
 		);
 	}
