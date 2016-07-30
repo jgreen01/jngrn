5a045f75f78f02a0683e9356e3a12c26374f0c0d
Jon Green <jgreen82@gatech.edu>
2016-07-30 16:10:13 -0400

    Switching site boilerplate to the react-pure-component-starter created by Eric Elliott.

--- old_site/source/javascript/index.js
+++
@@ -1,9 +0,0 @@
-$(".button-collapse").sideNav();
-
-$(".call-me").on("click", function() {
-	location.href = "tel:" + "1" + "202" + "525" + "7234";
-});
-
-$(".email-me").on("click", function() {
-	location.href = "mail" + "to:" + "green" + "bj@" + "umich." + "edu";
-});
